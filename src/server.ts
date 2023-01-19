1e2dff42dc716d25d2d4bc2cdd0747b69a8e85b5
Julio Zambom <work.juliozambom@gmail.com>
2023-01-19 12:25:40 -0300

    perf: setting up some puppeeteer flags to improve app performance

--- src/server.ts
+++ src/server.ts
@@ -136,7 +136,23 @@ mongoose.connect(String(process.env.DATABASE_URL))
           const store = new MongoStore({ mongoose: mongoose });
           const client = new Client({
               puppeteer: {
-                args: ['--no-sandbox', "--disable-setuid-sandbox"]
+                args: [
+                  '--start-maximized',
+                  '--no-default-browser-check',
+                  '--disable-infobars', 
+                  '--disable-web-security',
+                  '--disable-site-isolation-trials',
+                  '--no-experiments',
+                  '--ignore-gpu-blacklist',
+                  '--ignore-certificate-errors',
+                  '--ignore-certificate-errors-spki-list',
+                  '--disable-gpu',
+                  '--disable-extensions',
+                  '--disable-default-apps',
+                  '--enable-features=NetworkService',
+                  '--disable-setuid-sandbox',
+                  '--no-sandbox'
+                ]
               },
               authStrategy: new RemoteAuth({
                 store: store,
